1190c9a559a5ac423e42357c35a4d9dc090e8a7b
Harris Brakmic <brakmic@web.de>
2017-02-24 11:54:14 +0100

    use unique_ptr

--- ShipCloud/ShipCloud/string-helper.cpp
+++ ShipCloud/ShipCloud/string-helper.cpp
@@ -4,13 +4,13 @@ const std::string shipcloud::api::helpers::StringHelper::to_string(const std::ws
 {
 	const auto pwstr = wstr.c_str();
 	const size_t len = wstr.length();
-	auto pstr = new char[len];
-	const size_t cplen = wcstombs(pstr, pwstr, len);
-	pstr[len] = 0;
+	auto pstr = std::unique_ptr<char[]>{ new char[len] };
+	const size_t cplen = wcstombs(pstr.get(), pwstr, len);
+	pstr.get()[len] = 0;
 	if (cplen != len) {
 		throw new std::exception("Could not convert to std::string");
 	}
-	return std::string(std::move(pstr));
+	return std::string(std::move(pstr ? pstr.release() : ""));
 }
 
 const std::wstring shipcloud::api::helpers::StringHelper::to_wstring(const std::string& str)
@@ -18,11 +18,11 @@ const std::wstring shipcloud::api::helpers::StringHelper::to_wstring(const std::
 	const auto pstr = str.c_str();
 	const size_t len = str.length();
 	const size_t size = len * sizeof(wchar_t);
-	auto pwstr = new wchar_t[size];
-	const size_t cplen = mbstowcs(pwstr, pstr, size);
-	pwstr[size] = 0;
+	auto pwstr = std::unique_ptr<wchar_t[]>{ new wchar_t[size] };
+	const size_t cplen = mbstowcs(pwstr.get(), pstr, size);
+	pwstr.get()[size] = 0;
 	if (cplen != len) {
 		throw new std::exception("Could not convert to std::wstring");
 	}
-	return std::wstring(std::move(pwstr));
+	return std::wstring(std::move(pwstr ? pwstr.release() : L""));
 }
